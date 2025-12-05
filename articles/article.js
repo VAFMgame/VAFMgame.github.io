243a276b30a2fcbf7cfdd357fdf0021b11fa8349
VAFMgame <hugo.valet@outlook.fr>
2025-12-05 11:44:20 +0100

    Update article.js

--- articles/article.js
+++ articles/article.js
@@ -1,1 +1,10 @@
+  function copyArticleLink() {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url).then(() => {
+      alert("Lien copié dans le presse-papiers !");
+    }).catch(err => {
+      alert("Erreur lors de la copie du lien.");
+    });
+  }
+
 
