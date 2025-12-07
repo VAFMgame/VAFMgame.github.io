a8933447ea0817e1dcfd10e382f39c2f2084894f
VAFMgame <hugo.valet@outlook.fr>
2025-12-07 14:08:48 +0100

    Update recommandation.js

--- articles/recommandation.js
+++ articles/recommandation.js
@@ -1,1 +1,13 @@
+  const titreArticle = "Elden Ring: Firestorm Hollow"; // ← change ça selon l’article
+
+  function enregistrerArticleVu(titre) {
+    const vus = JSON.parse(localStorage.getItem("articlesVus") || "[]");
+    if (!vus.includes(titre)) {
+      vus.push(titre);
+      localStorage.setItem("articlesVus", JSON.stringify(vus));
+    }
+  }
+
+  enregistrerArticleVu(titreArticle);
+
 
